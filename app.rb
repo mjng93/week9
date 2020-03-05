e2937f0888af4b09569c46269d768ddf289c48d0
Michael Ng <michael.joseph.ng@gmail.com>
2020-03-05 01:12:02 +0000

    First edits

--- app.rb
+++ app.rb
@@ -46,6 +46,16 @@ end
 get "/events/:id/rsvps/create" do
     puts "params: #{params}"
 
+    @event = events_table.where(id: params[:id]).to_a[0]
+    
+    rsvps_table.insert(
+    event_id: @event[:id],
+    name: params["name"],
+    email: params["email"],
+    comments: params["comments"],
+    going: params["going"]
+    )
+
     view "create_rsvp"
 end
 
@@ -72,3 +82,5 @@ end
 get "/logout" do
     view "logout"
 end
+
+puts "success"
